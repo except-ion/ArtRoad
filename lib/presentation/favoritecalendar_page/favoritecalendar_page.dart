581caf52c271f13132671604676bbdd3de89dd1b
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2023-08-28 21:44:01 +0900

    r[refactorÂ] favoritecalendar_page folder ã  modify & file add

--- lib/presentation/favoritecalendar_page/favoritecalendar_page.dart
+++ lib/presentation/favoritecalendar_page/favoritecalendar_page.dart
@@ -2,6 +2,7 @@ import 'package:artroad/core/app_export.dart';
 import 'package:flutter/material.dart';
 import '../mycalendar_page/mycalendar_tcs.dart';
 import 'favoritecalendar_tcs.dart';
+import 'max_num_of_weeks.dart';
 
 class FavoritecalendarPage extends StatefulWidget {
   const FavoritecalendarPage({Key? key}) : super(key: key);
@@ -12,6 +13,14 @@ class FavoritecalendarPage extends StatefulWidget {
 
 class _FavoritecalendarPageState extends State<FavoritecalendarPage> {
   bool isTableCalendarVisible = true; // 초기에는 TableCalendar 보이도록
+  bool isFiveWeeks = MaxNumOfWeeks().calculateMaxWeeksInMonth(DateTime.now());
+
+  // isFiveWeeks 변수 업데이트 메서드
+  void updateIsFiveWeeks(bool newValue) {
+    setState(() {
+      isFiveWeeks = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +33,33 @@ class _FavoritecalendarPageState extends State<FavoritecalendarPage> {
           child: Container(
             height: getVerticalSize(836),
             width: double.maxFinite,
-            padding: getPadding(top: 38, bottom: 38),
+            //padding: getPadding(top: 38, bottom: 38),
             child: Column(
               children: [
-                // 로고
+                // logo
                 Align(
                   alignment: Alignment.topLeft,
                   child: Padding(
-                    padding: getPadding(left: 34),
-                    child: Text(
-                      "ArtRoad",
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                      style: theme.textTheme.displaySmall,
+                    padding: getPadding(top: 30, left: 30),
+                    child: ShaderMask(
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [Colors.transparent, Colors.white], // 그라데이션 색상 설정
+                          stops: [0.0, 1.0], // 그라데이션 위치 설정
+                        ).createShader(bounds);
+                      },
+                      child: Text(
+                        "ArtRoad",
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                        style: theme.textTheme.displaySmall?.copyWith(
+                          fontSize: 30.0, // 폰트 사이즈 조절
+                          fontWeight: FontWeight.normal, // 폰트 굵기 조절
+                          color: Colors.grey, // 텍스트 색상 설정 (그라데이션에 의해 가려지므로 원하는 색상 사용 가능)
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -59,15 +82,15 @@ class _FavoritecalendarPageState extends State<FavoritecalendarPage> {
                         ),
                       ),
                       child: Stack(
-                          children: [
-                            CustomImageView(
-                              imagePath: ImageConstant.imgMonthinlove,
-                              height: getSize(26),
-                              width: getSize(26),
-                              alignment: Alignment.center,
-                            ),
-                          ],
-                        ),
+                        children: [
+                          CustomImageView(
+                            imagePath: ImageConstant.imgMonthinlove,
+                            height: getSize(28),
+                            width: getSize(28),
+                            alignment: Alignment.center,
+                          ),
+                        ],
+                      ),
                     ),
 
                     // My Calendar Button
@@ -86,15 +109,15 @@ class _FavoritecalendarPageState extends State<FavoritecalendarPage> {
                         ),
                       ),
                       child: Stack(
-                          children: [
-                            CustomImageView(
-                              imagePath: ImageConstant.imgToday,
-                              height: getSize(26),
-                              width: getSize(26),
-                              alignment: Alignment.center,
-                            ),
-                          ],
-                        ),
+                        children: [
+                          CustomImageView(
+                            imagePath: ImageConstant.imgToday,
+                            height: getSize(28),
+                            width: getSize(28),
+                            alignment: Alignment.center,
+                          ),
+                        ],
+                      ),
                     )
                   ],
                 ),
@@ -102,25 +125,29 @@ class _FavoritecalendarPageState extends State<FavoritecalendarPage> {
                   child: Stack(
                     alignment: Alignment.topCenter,
                     children: [
-                      CustomImageView(
-                        imagePath: ImageConstant.imgCalendar,
-                        height: getVerticalSize(470),
-                        width: getHorizontalSize(428),
-                        radius: BorderRadius.circular(getHorizontalSize(8)),
-                        alignment: Alignment.topCenter,
+                      Transform.scale(
+                        scale: 1.0,
+                        // scale: isFiveWeeks ? 1.0 : 1.5,
+                        child: CustomImageView(
+                          imagePath: ImageConstant.imgCalendar,
+                          alignment: Alignment.topCenter,
+                        ),
                       ),
                       if (isTableCalendarVisible)
                         Positioned(
-                          top: -15,
-                          left: 20,
-                          right: 20,
-                          child: fTableCalendarScreen(), // isTableCalendarVisible가 true인 경우에는 fTableCalendarScreen() 호출
+                          top: -10,
+                          left: 10,
+                          right: 10,
+                          child: fTableCalendarScreen(
+                              isFiveWeeks: isFiveWeeks,
+                              updateIsFiveWeeks: updateIsFiveWeeks
+                          ), // isTableCalendarVisible가 true인 경우에는 fTableCalendarScreen() 호출
                         ),
                       if (!isTableCalendarVisible)
                         Positioned(
-                          top: -15,
-                          left: 20,
-                          right: 20,
+                          top: -10,
+                          left: 10,
+                          right: 10,
                           child: mTableCalendarScreen(), // isTableCalendarVisible가 false인 경우에는 mTableCalendarScreen() 호출
                         ),
                     ],
