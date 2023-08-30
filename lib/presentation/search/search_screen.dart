afe5fd1816e3659348feb19061ae35f319cb0d3c
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2023-08-31 03:15:22 +0900

    [test] 공연상세 페이지 테스트 버튼 임시 추가

--- lib/presentation/search/search_screen.dart
+++ lib/presentation/search/search_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../facility/facilitydetail_screen.dart';
+
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
@@ -10,6 +12,19 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: InkWell(
+        onTap: () {
+          // 버튼을 누를 때 다른 페이지로 이동하는 코드 작성
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FacilityDetailScreen()), // 다른페이지이름에는 이동하고 싶은 페이지의 이름이 들어가야 합니다.
+          );
+        },
+        child: Text(
+          '공연 상세',
+        ),
+      )
+    );
   }
 }
