6ef12b2bf09edf3341fa72aeed138e69e890e36e
suwith <suwith@kookmin.ac.kr>
2023-08-31 23:28:13 +0900

    [feat] 홈화면 전체 랭킹 커스텀

--- lib/presentation/home/home_ranking_items_tile.dart
+++ lib/presentation/home/home_ranking_items_tile.dart
@@ -1,46 +1,56 @@
 import 'package:artroad/presentation/home/home_ranking_items.dart';
 import 'package:flutter/material.dart';
 
-class RankingItemsTile extends StatelessWidget {
+class RankingItemsTile extends StatefulWidget {
   RankingItemsTile(this._RankingItems);
 
   final RankingItems _RankingItems;
+
+  @override
+  State<RankingItemsTile> createState() => _RankingItemsTileState();
+}
+
+class _RankingItemsTileState extends State<RankingItemsTile> {
+  bool _imageLoading = false; //network로 수정할 때 true로 바꾸기
+
+  void _updateImageLoading(bool isLoading) {
+    if (_imageLoading != isLoading) {
+      setState(() {
+        _imageLoading = isLoading;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          //Text('${rankingList[index].poster}'),
-          Container(
-            height: 10,
-            alignment: Alignment.center,
-            color: Colors.yellow,
-            child: Text(
-              '${_RankingItems.poster}',
-              style: TextStyle(
-                fontSize: 20,
+      height: 270,
+      alignment: Alignment.center,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: _imageLoading
+            ? CircularProgressIndicator()
+            : Image.asset(
+                '${widget._RankingItems.poster}',
+                errorBuilder: (context, error, stackTrace) {
+                  _updateImageLoading(false);
+                  return Text('이미지 로드 실패');
+                },
+                width: 200,
+                height: 270,
+                fit: BoxFit.fitWidth,
+                //아래는 z
+                // loadingBuilder: (context, child, loadingProgress) {
+                //   if (loadingProgress == null) {
+                //     _updateImageLoading(false);
+                //     return child;
+                //   } else {
+                //     _updateImageLoading(true);
+                //     return CircularProgressIndicator();
+                //   }
+                // },
               ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.lightGreenAccent,
-              alignment: Alignment.center,
-              child: Column(
-                children: [
-                  Text(
-                    '${_RankingItems.rnum}',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
-    
   }
 }
--- lib/presentation/home/home_ranking_swiper.dart
+++ lib/presentation/home/home_ranking_swiper.dart
@@ -8,74 +8,101 @@ class HomeRankingSwiper extends StatelessWidget {
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "1"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "2"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "3"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example3.jpg",
         rnum: "4"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example2.jpg",
         rnum: "5"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example3.jpg",
         rnum: "6"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example4.jpg",
         rnum: "7"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "8"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example2.jpg",
         rnum: "9"),
+    RankingItems(
+        mt20id: "aaa",
+        cate: "연극",
+        poster: "assets/images/poster_example2.jpg",
+        rnum: "10"),
   ];
-
   @override
   Widget build(BuildContext context) {
-    print(rankingList.elementAt(0).rnum);
     return Container(
-            height: 300, // 스와이퍼의 높이 설정해야함
-            child: Swiper(
-              onTap: (index) => {}, //사진 클릭시 이동
-              autoplay: true,
-              scale: 0.8,
-              viewportFraction: 0.6,
-              itemCount: rankingList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return RankingItemsTile(rankingList[index]);
-              },
+      height: 300, // 스와이퍼의 높이 설정해야함
+      child: Swiper(
+        onTap: (index) => {}, //사진 클릭시 이동
+        autoplay: true,
+        scale: 0.8,
+        viewportFraction: 0.6,
+        itemCount: rankingList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Stack(children: [
+            RankingItemsTile(
+              rankingList[index],
             ),
-          );
+            Transform.translate(
+              offset: Offset(10, 175),
+              child: Text(
+                '${rankingList[index].rnum}',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 110,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: -13,
+                    shadows: [
+                      Shadow(
+                          // bottomLeft
+                          offset: Offset(-1.5, -1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // bottomRight
+                          offset: Offset(1.5, -1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // topRight
+                          offset: Offset(1.5, 1.5),
+                          color: Colors.black),
+                      Shadow(
+                          // topLeft
+                          offset: Offset(-1.5, 1.5),
+                          color: Colors.black),
+                    ]),
+              ),
+            ),
+          ]);
+        },
+      ),
+    );
   }
 }
