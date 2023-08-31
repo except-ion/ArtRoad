e6888a4219782d4deaf0c41848d984b4eba53541
suwith <suwith@kookmin.ac.kr>
2023-09-01 08:30:21 +0900

    Revert "Feature/calendar"

--- lib/presentation/calendar/day_of_week.dart
+++
@@ -1,8 +0,0 @@
-class DayOfWeek {
-  String getDayOfWeek(DateTime date) {
-    final List<String> daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
-
-    if (date.weekday == 7 ) return daysOfWeek[0]; // 일요일의 경우 0 인덱스를 사용
-    else return daysOfWeek[date.weekday];
-  }
-}
