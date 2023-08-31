d0cdc0e241d407207ee3c3db4aff8202f229f25f
suwith <suwith@kookmin.ac.kr>
2023-08-31 23:29:07 +0900

    [feat] 홈화면 카테고리별 랭킹 그리드뷰 구현

--- lib/presentation/home/home_ranking_grid_view.dart
+++ lib/presentation/home/home_ranking_grid_view.dart
@@ -7,77 +7,71 @@ class HomeRankingGridView extends StatelessWidget {
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "1"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "2"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "3"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example3.jpg",
         rnum: "4"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example2.jpg",
         rnum: "5"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example3.jpg",
         rnum: "6"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example4.jpg",
         rnum: "7"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example1.jpg",
         rnum: "8"),
     RankingItems(
         mt20id: "aaa",
         cate: "연극",
-        poster:
-            "https://www.kopis.or.kr/upload/pfmPoster/PF_PF131793_160603_140415.jpg",
+        poster: "assets/images/poster_example2.jpg",
         rnum: "9"),
   ];
-
   @override
   Widget build(BuildContext context) {
-    print(rankingList.elementAt(0).rnum);
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: GridView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: rankingList.length, //item 개수
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-          childAspectRatio: 1 / 2, //item 의 가로 1, 세로 2 의 비율
-          mainAxisSpacing: 10, //수평 Padding
-          crossAxisSpacing: 10, //수직 Padding
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: SizedBox(
+        height: 540,
+        child: GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: rankingList.length, //item 개수
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+            childAspectRatio: 3 / 4, //item 의 가로 1, 세로 2 의 비율
+            mainAxisSpacing: 5, //수평 Padding
+            crossAxisSpacing: 5, //수직 Padding
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            //item 의 반목문 항목 형성
+            return Padding(
+                padding: EdgeInsets.all(5),
+                child: RankingItemsTile(rankingList[index]));
+          },
         ),
-        itemBuilder: (BuildContext context, int index) {
-          //item 의 반목문 항목 형성
-          return RankingItemsTile(rankingList[index]);
-        },
       ),
     );
   }
