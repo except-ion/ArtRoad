1250051f133418e191bbf874a394399a3ce9cb00
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2023-09-04 22:31:19 +0900

    [feat] 아트로드 정보 페이지 구현

---
+++ lib/presentation/profile/artroadinfo/artroad_info.dart
@@ -0,0 +1,26 @@
+import 'package:artroad/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+import '../../../widgets/custom_header.dart';
+
+class ArtRoadInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CustomHeader(
+            name: 'About ArtRoad',
+          ),
+          CustomImageView(
+            imagePath: ImageConstant.imgAnnounce1,
+          ),
+          Divider(),
+          Text(
+            '어쩌구 저쩌구......하......피곤띠'
+          ),
+        ],
+      ),
+    );
+  }
+}
