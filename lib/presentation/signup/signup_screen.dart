e8e4cd7e18474964d8449c04dcdb7e4d91c20d1c
suwith <suwith@kookmin.ac.kr>
2023-09-06 00:08:18 +0900

    [refactor] 유효성 검사 위젯 분리

--- lib/presentation/signup/signup_screen.dart
+++ lib/presentation/signup/signup_screen.dart
@@ -2,9 +2,9 @@ import 'package:artroad/presentation/signup/signup_terms_of_service.dart';
 import 'package:artroad/widgets/custom_header.dart';
 import 'package:artroad/widgets/custom_textformfield.dart';
 import 'package:artroad/widgets/custom_button_main_color.dart';
-
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+
+import '../../widgets/check_validate.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -154,60 +154,3 @@ class _SignupScreenState extends State<SignupScreen> {
   }
 }
 
-class CheckValidate {
-  //이름 유효성 검사
-  String? validateName(FocusNode focusNode, String value) {
-    if (value.isEmpty) {
-      focusNode.requestFocus();
-      return '이름을 입력하세요.';
-    } else {
-      return null;
-    }
-  }
-
-  String? validateEmail(FocusNode focusNode, String value) {
-    if (value.isEmpty) {
-      focusNode.requestFocus();
-      return '이메일을 입력하세요.';
-    } else {
-      Pattern pattern =
-          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-      RegExp regExp = new RegExp('$pattern');
-      if (!regExp.hasMatch(value)) {
-        focusNode.requestFocus(); //포커스를 해당 textformfield에 맞춘다.
-        return '잘못된 이메일 형식입니다.';
-      } else {
-        return null;
-      }
-    }
-  }
-
-  String? validatePassword(FocusNode focusNode, String value) {
-    if (value.isEmpty) {
-      focusNode.requestFocus();
-      return '비밀번호를 입력하세요.';
-    } else {
-      Pattern pattern =
-          r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?~^<>,.&+=])[A-Za-z\d$@$!%*#?~^<>,.&+=]{8,15}$';
-      RegExp regExp = new RegExp('$pattern');
-      if (!regExp.hasMatch(value)) {
-        focusNode.requestFocus();
-        return '특수문자, 대소문자, 숫자 포함 8자 이상 15자 이내로 입력하세요.';
-      } else {
-        return null;
-      }
-    }
-  }
-
-  String? validatePwCheck(FocusNode focusNode, String value, String pw) {
-    if (value.isEmpty) {
-      focusNode.requestFocus();
-      return '비밀번호를 한 번 더 입력하세요.';
-    } else if (value != pw) {
-      focusNode.requestFocus();
-      return '비밀번호가 일치하지 않습니다.';
-    } else {
-      return null;
-    }
-  }
-}
