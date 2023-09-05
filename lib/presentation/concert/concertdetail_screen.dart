15e396a3668d574f5a643d4b36e09dda5446db56
suwith <suwith@kookmin.ac.kr>
2023-09-06 02:37:49 +0900

    [feat] 공연 상세화면 null 값 처리

--- lib/presentation/concert/concertdetail_screen.dart
+++ lib/presentation/concert/concertdetail_screen.dart
@@ -16,11 +16,11 @@ class _ConcertDetailScreenState extends State<ConcertDetailScreen> {
   ConcertDetail concert = ConcertDetail(
       mt20id: 'PF132236',
       prfnm: '멤피스',
-      poster: 'assets/images/poster_example5.jpeg',
-      prfpdfrom: '2023.07.12',
+      poster: 'null',
+      prfpdfrom: null,
       prfpdto: '2023.09.03',
       prfruntime: '165분',
-      prfage: '14세 이상 관람가',
+      prfage: null,
       genrenm: '뮤지컬',
       fcltynm: '예술의 전당 오페라 극장',
       styurls: 'assets/images/concert_example.jpg');
@@ -45,12 +45,12 @@ class _ConcertDetailScreenState extends State<ConcertDetailScreen> {
                             width: double.infinity,
                             child: Padding(
                               padding: EdgeInsets.only(
-                                  top: 90, left: 30, right: 30, bottom: 30),
+                                  top: 90, left: 30, right: 30, bottom: 50),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    '${concert.prfnm}',
+                                    concert.prfnm ?? '제공된 정보가 없습니다.',
                                     style: TextStyle(fontSize: 30),
                                   ),
                                   SizedBox(
@@ -58,24 +58,27 @@ class _ConcertDetailScreenState extends State<ConcertDetailScreen> {
                                   ),
                                   CustomDetailText(
                                     type: '공연기간',
-                                    content:
-                                        '${concert.prfpdfrom} ~ ${concert.prfpdto}',
+                                    content: (concert.prfpdfrom == null &&
+                                            concert.prfpdto == null)
+                                        ? '제공된 정보가 없습니다.'
+                                        : '${concert.prfpdfrom ?? '정보 없음'} ~ ${concert.prfpdto ?? '정보 없음'}',
                                   ),
                                   CustomDetailText(
                                     type: '공연시간',
-                                    content: '${concert.prfruntime}',
+                                    content:
+                                        concert.prfruntime ?? '제공된 정보가 없습니다.',
                                   ),
                                   CustomDetailText(
                                     type: '관람연령',
-                                    content: '${concert.prfage}',
+                                    content: concert.prfage ?? '제공된 정보가 없습니다.',
                                   ),
                                   CustomDetailText(
                                     type: '장르',
-                                    content: '${concert.genrenm}',
+                                    content: concert.genrenm ?? '제공된 정보가 없습니다.',
                                   ),
                                   CustomDetailText(
                                     type: '공연장',
-                                    content: '${concert.fcltynm}',
+                                    content: concert.fcltynm ?? '제공된 정보가 없습니다.',
                                     onPressed: () {
                                       Navigator.push(
                                         context,
@@ -92,23 +95,30 @@ class _ConcertDetailScreenState extends State<ConcertDetailScreen> {
                                   SizedBox(
                                     height: 20,
                                   ),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        '상세 정보',
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
+                                  Center(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '상세 정보',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500,
+                                          ),
                                         ),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.only(top: 40),
-                                        child:
-                                            Image.asset('${concert.styurls}'),
-                                      )
-                                    ],
+                                        Padding(
+                                          padding: EdgeInsets.only(top: 40,bottom: 40),
+                                          child: concert.styurls == null
+                                              ? Image.asset(
+                                                  '${concert.styurls}')
+                                              : Text('제공된 정보가 없습니다.'),
+                                        )
+                                      ],
+                                    ),
                                   )
                                 ],
                               ),
@@ -119,10 +129,35 @@ class _ConcertDetailScreenState extends State<ConcertDetailScreen> {
                           offset: Offset(30, 65),
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(16),
-                            child: Image.asset(
-                              '${concert.poster}',
-                              width: 170,
-                            ),
+                            child: (concert.poster == null)
+                                ? Image.asset(
+                                    '${concert.poster}',
+                                    width: 170,
+                                  )
+                                : Container(
+                                    color: Colors.grey[100],
+                                    width: 170,
+                                    height: 240,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.no_photography_outlined,
+                                          size: 50,
+                                          color: Colors.grey[600],
+                                        ),
+                                        Text(
+                                          'No Image',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey[600],
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                           ),
                         ),
                       ],
