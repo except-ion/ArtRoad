e012ff38d56c614368a50d8a27b7ad1a02dbea48
suwith <suwith@kookmin.ac.kr>
2023-09-07 03:07:08 +0900

    [design] 커스텀 헤더 구조 수정

--- lib/widgets/custom_header.dart
+++ lib/widgets/custom_header.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CustomHeader extends StatefulWidget {
@@ -6,7 +7,7 @@ class CustomHeader extends StatefulWidget {
   final bool isDetail;
 
   const CustomHeader(
-      {Key? key, this.name, this.hasLiked = false, this.isDetail = false});
+      {Key? key, this.name = '', this.hasLiked = false, this.isDetail = false});
 
   @override
   _CustomHeaderState createState() => _CustomHeaderState();
@@ -32,6 +33,7 @@ class _CustomHeaderState extends State<CustomHeader> {
   Widget build(BuildContext context) {
     print(widget.isDetail);
     return Container(
+      height: 70,
       child: Stack(
         children: [
           widget.isDetail
@@ -40,72 +42,75 @@ class _CustomHeaderState extends State<CustomHeader> {
           Padding(
             padding: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
             child: Stack(
-              alignment: Alignment.center,
               children: [
                 Align(
-                  alignment: Alignment.topLeft,
-                  child: Transform.scale(
-                    scale: 1.5,
-                    child: IconButton(
-                      icon: FittedBox( // 아이콘에 FittedBox 적용
-                        fit: BoxFit.contain,
-                        child: Icon(
-                          Icons.arrow_back_rounded,
-                          color: widget.isDetail ? Colors.white : Color(0xFF00233D),
-                        ),
+                  alignment: Alignment.center,
+                  child: FittedBox(
+                    // 텍스트에 FittedBox 적용
+                    fit: BoxFit.fitHeight,
+                    child: AutoSizeText(
+                      widget.name!,
+                      style: TextStyle(
+                        color: widget.isDetail ? Colors.white : Colors.black,
+                        fontSize: 21,
                       ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      maxFontSize: 21,
                     ),
                   ),
                 ),
-                if (widget.name != null)
-                  Positioned(
-                    top: 14,
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: FittedBox( // 텍스트에 FittedBox 적용
-                        fit: BoxFit.fitHeight,
-                        child: Text(
-                          widget.name!,
-                          style: TextStyle(
-                            color: widget.isDetail ? Colors.white : Colors.black,
-                            fontSize: 21,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8,bottom: 8),
+                      child: Transform.scale(
+                        alignment: Alignment.center,
+                        scale: 1.5,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_rounded,
+                            color: widget.isDetail
+                                ? Colors.white
+                                : Color(0xFF00233D),
                           ),
-                          textAlign: TextAlign.center,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
                         ),
                       ),
                     ),
-                  ),
-                if (widget.hasLiked)
-                  Positioned(
-                    right: 0,
-                    top: 0,
-                    child: Transform.scale(
-                      scale: 1.5,
-                      child: IconButton(
-                        icon: FittedBox( // 아이콘에 FittedBox 적용
-                          fit: BoxFit.contain,
-                          child: isLiked
-                              ? Icon(
-                                  //좋아요 클릭 전
-                                  Icons.favorite_border,
-                                  color: widget.isDetail ? Colors.white : Color(0xFF00233D),
-                                )
-                              : Icon(
-                                  //좋아요 클릭 후
-                                  Icons.favorite,
-                                  color: Colors.red,
-                                ),
+                    if (widget.hasLiked)
+                      Transform.scale(
+                        scale: 1.5,
+                        child: IconButton(
+                          icon: FittedBox(
+                            // 아이콘에 FittedBox 적용
+                            fit: BoxFit.contain,
+                            child: isLiked
+                                ? Icon(
+                                    //좋아요 클릭 전
+                                    Icons.favorite_border,
+                                    color: widget.isDetail
+                                        ? Colors.white
+                                        : Color(0xFF00233D),
+                                  )
+                                : Icon(
+                                    //좋아요 클릭 후
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                  ),
+                          ),
+                          onPressed: toggleLiked,
                         ),
-                        onPressed: toggleLiked,
                       ),
-                    ),
-                  ),
+                  ],
+                ),
               ],
             ),
-          ),
+          )
         ],
       ),
     );
